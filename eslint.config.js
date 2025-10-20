c261bb088c27f66ec1a3a488a4d68aaf3bb20a00
juhanPark <iiiamst16@gmail.com>
2025-10-21 01:12:29 +0900

    chore: eslint 관련 라이브러리와 설정 수정

--- eslint.config.js
+++ eslint.config.js
@@ -4,20 +4,27 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import plugin from 'eslint-plugin-react'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    rules: {
+      'quotes': ['error', 'double'],
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      plugin.configs.flat['jsx-runtime'],
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
+  eslintConfigPrettier
 ])
